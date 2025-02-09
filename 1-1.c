6873fa1470c673a9acafbbf4b9e1f967deb11d89
yunative <47847960+yi7242@users.noreply.github.com>
2025-02-09 17:42:31 +0900

    complete 1-1

---
+++ 1-1.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "pthread.h"
+
+void *thread1() {
+  printf("Hello, World! from thread #1\n");
+  return 0;
+}
+void *thread2() {
+  printf("Hello, World! from thread #2\n");
+  return 0;
+}
+
+int main(void) {
+  pthread_t pthread1, pthread2;
+  pthread_create( &pthread1, NULL, &thread1, NULL);
+  pthread_create( &pthread2, NULL, &thread2, NULL);
+  pthread_join(pthread1, NULL);
+  pthread_join(pthread2, NULL);
+  return 0;
+}
